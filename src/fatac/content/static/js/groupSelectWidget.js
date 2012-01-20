896338db5e83742d61f2a701f9295769f3f12f1d
Ferran Pons <ferran.pons.sanche@upcnet.es>
2012-01-20 13:01:05 +0100

    Widget funcionant

--- src/fatac/content/static/js/groupSelectWidget.js
+++ src/fatac/content/static/js/groupSelectWidget.js
@@ -17,7 +17,7 @@ function groups_addNewKeyword(toList, newText, newValue) {
   theToList[theLength].value = newValue;
 }
 
-function groups_moveKeywords(fromList,toList,selectThese) {
+function groups_moveKeywords(fromList,toList,selectThese, forSubmit) {
   theFromList=document.getElementById(fromList);
   for (var x=0; x < theFromList.length; x++) {
     if (theFromList[x].selected) {
@@ -31,4 +31,40 @@ function groups_moveKeywords(fromList,toList,selectThese) {
     }
   }
   groups_selectAllWords(selectThese);
+  copyDataForSubmit(forSubmit);
+}
+
+// copy each item of "toSel" into one hidden input field
+function copyDataForSubmit(name)
+{
+  // shortcuts for selection field and hidden data field
+  var toSel = document.getElementById(name);
+  var toDataContainer = document.getElementById(name+"-toDataContainer");
+
+  // delete all child nodes (--> complete content) of "toDataContainer" span
+  while (toDataContainer.hasChildNodes())
+      toDataContainer.removeChild(toDataContainer.firstChild);
+
+  // create new hidden input fields - one for each selection item of
+  // "to" selection
+  for (var i = 0; i < toSel.options.length; i++)
+  {
+    // create virtual node with suitable attributes
+    var newNode = document.createElement("input");
+    var newAttr = document.createAttribute("name");
+    name = name.replace('_to','');
+    newAttr.nodeValue = name.replace(/-/g, '.')+':list';
+    newNode.setAttributeNode(newAttr);
+
+    newAttr = document.createAttribute("type");
+    newAttr.nodeValue = "hidden";
+    newNode.setAttributeNode(newAttr);
+
+    newAttr = document.createAttribute("value");
+    newAttr.nodeValue = toSel.options[i].value;
+    newNode.setAttributeNode(newAttr);
+
+    // actually append virtual node to DOM tree
+    toDataContainer.appendChild(newNode);
+  }
 }
