1fce053f66b0e88c0a9b12a8018ade1e93acdc6d
Ferran Pons <ferran.pons.sanche@upcnet.es>
2012-01-27 11:59:27 +0100

    Actualitzat

--- src/fatac/content/static/js/playlist.js
+++ src/fatac/content/static/js/playlist.js
@@ -2,8 +2,69 @@ $(document).ready(function() {
     var pathname = '';
     var order = [];
 
+    pathname = getPath();
+
+    // Assign handlers immediately after making the request,
+    // and remember the jqxhr object for this request
+    if ((window.location.pathname.indexOf("edit") == -1) && 
+        (window.location.pathname.indexOf("@@edit") == -1))
+    {
+        var resultatsPerPagina = 66;
+
+        if (pathname.indexOf("sortingView") != -1)
+        {
+            pathname = pathname.split("sortingView")[0];
+            resultatsPerPagina = 400;
+        }
+
+        $.getJSON(pathname + "returnOrderedList", function() { 
+        })
+        .error(function() { alert("No s'ha pogut carregar la llista d'objectes"); })
+        .complete(function( data ) {
+
+            var llista_ids = ['Expedient_Ana_Mendieta', 'Expedient_Antoni_Tapies_Els_llocs_de_lart'];
+
+            llista_ids = jQuery.parseJSON( data.responseText );
+
+            parametres_visualitzacio = {llista_ids: llista_ids['Ids'], visualitzacio: 'imatge', zoom: '1', pagina_actual: 1, resultats_per_pagina: resultatsPerPagina};
+
+            $('#visual-portal-wrapper').get(0).parametres_visualitzacio = parametres_visualitzacio;
+
+            parametres_visualitzacio_json = JSON.stringify(parametres_visualitzacio);
+
+            var currentView = '';
+            if (window.location.pathname.indexOf("sortingView") != -1)
+            {
+                currentView = 'orderPlaylistView';
+            }
+            else
+            {
+                currentView = 'playlistView';
+            }
+
+            $.post(pathname + currentView, {parametres_visualitzacio: parametres_visualitzacio_json}, function(data){
+                $('div#zona_resultats').replaceWith(data);
+            })        
+            .complete(function( data ){
+                // The sortable list of objects
+                if (currentView == 'orderPlaylistView')
+                {
+                    $('#sortable').sortable({
+                        update: function() {        
+                            pathname = getPath();
+                            order = getOrder();
+
+                            updateList(pathname.split('sortingView')[0], order);
+                        }                                         
+                    });
+                }
+            });
+        });
+    }
+
+/*
     // The sortable list of objects
-    $('#sortable').sortable({ 
+    $('#sortable').sortable({
         update: function() {        
             pathname = getPath();
             order = getOrder();
@@ -11,7 +72,7 @@ $(document).ready(function() {
             updateList(pathname, order);
         }                                         
     });
-
+*/
     // Check and Uncheck all the selection
     $("#forRemove_all").click(function() 
     {
@@ -96,3 +157,29 @@ function updateList(pathname, order)
 }
 
 
+function removeSelectedObject(objectId, tagObjectId)
+{
+    //Get the current path
+    var pathname = getPath();
+
+    //Parse for different paths
+    if (pathname.indexOf("sortingView") != -1)
+    {
+        pathname = pathname.split("sortingView")[0];
+    }
+    else if (pathname[-1] != '/')
+    {
+        pathname = pathname + '/';
+    }
+
+    path = pathname + "deleteObjectId?objectId=" + objectId; //tagObjectId.replace("#","");
+
+    $.ajax({url: path,
+            type: "post",
+            error: function() { alert("No s'ha eliminar l'objecte de la playlist"); },
+            success: function() { //window.location.href = pathname + "sortingView";
+                                    $(tagObjectId).remove();
+                                },
+          });
+    
+}
