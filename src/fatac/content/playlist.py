d52db09b668dee65e99e3f0d49b414f0c1d26956
Ferran Pons <ferran.pons.sanche@upcnet.es>
2012-01-27 11:55:10 +0100

    S'han afegit els canvis gaireb\303\251 definitius de la playlist

--- src/fatac/content/playlist.py
+++ src/fatac/content/playlist.py
@@ -25,6 +25,14 @@ from z3c.form import button, field
 from groupSelectWidget import GroupsSelectionWidgetFactory
 from plone.indexer import indexer
 
+from fatac.theme.browser.funcionsCerca import funcionsCerca
+from fatac.theme.browser.genericView import genericView
+from fatac.theme.browser.vistesCerca import resultatsView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+import json
+from plone.app.layout.viewlets.interfaces import IHtmlHead
+import urllib2
+import math
 
 
 class IPlaylist(form.Schema, IImageScaleTraversable):
@@ -42,19 +50,10 @@ class IPlaylist(form.Schema, IImageScaleTraversable):
    # form.write_permission(track="fatac.ModRelField")
    # form.read_permission(track="fatac.ModRelField")
 
-    # Llista d'obectes relacionats amb la Playlist
-    listToPlay = RelationList(
-        title = _(u"List To Play"),
-        description = _(u"This is a playlist to play"),
-        default=[],
-        value_type=RelationChoice(title=_(u"Objects to Play"), source=ObjPathSourceBinder()),
-        required = False,
-        )
-
     # Llista d'objectes de la playlist que serveix per ordenar
     orderedList = schema.List(
         title = _(u"Ordered List To Play"),
-        description = _(u"This is a list that has the order of the listToPlay field"),
+        description = _(u"This is a list that has the order of the DB objects"),
         default = [],
         value_type=schema.TextLine(),
         required = False,
@@ -70,7 +69,7 @@ class IPlaylist(form.Schema, IImageScaleTraversable):
         )
 
     #Amaguem els camps a l'edicio
-    form.omitted('listToPlay','orderedList')
+    form.omitted('orderedList')
 
 
 
@@ -88,53 +87,222 @@ grok.global_adapter(vigListIndexer, name="visibleInGroupsList")
 
 
 @grok.subscribe(IPlaylist, IObjectAddedEvent)
-def copyToOrderedList(playlist, event):
+def autoPublishPlaylist(playlist, event):
     """
-        Metode que afegeix un objecte a la llista que es pot ordenar de la Playlist
+        Publica la Playlist un cop s'ha creat.
     """
-    orderedlist = []
-    order = 0
-    for item in playlist.listToPlay:
-        orderedlist.append([order, item.to_object.getId()])
-        order += 1
-
-    playlist.orderedList = orderedlist
+    wtool = getToolByName(playlist, 'portal_workflow')
+    wtool.doActionFor(playlist, "publish")
 
 
-#myct1.setMyReferenceField(areferenceableobject)
-#myct1.getMyReferenceField()
 
-def set(self, value):
+
+class returnOrderedList(grok.View):
     """
-        Sets the relationship target. Monkeypatches issues in original
-        RelationListDataManager where manager assumes that every object has
-        intid.
+       Async View
+
+       Retorna la llista d'objectes que te la playlist per poder ordenar
     """
 
-    value = value or []
-    new_relationships = []
-    intids = getUtility(IIntIds)
-    for item in value:
-        try:
-            to_id = intids.getId(item)
-        except KeyError:
-            to_id = intids.register(item)
-        new_relationships.append(RelationValue(to_id))
-    super(RelationListDataManager, self).set(new_relationships)
+    grok.context(IPlaylist)
+    grok.require('zope2.View')
+    grok.name('returnOrderedList')
 
-RelationListDataManager.set = set
+    def render(self):
+        """
+            Render the settings as inline Javascript object in HTML <head>
+        """
+
+        request = self.request
+        context = self.context
+
+        settings = self.llistaObjectes(request, context)
+        json_snippet = json.dumps(settings)
+
+        request.response.setHeader("content-type", "application/json")
+        return json_snippet 
+
+    @memoize
+    def llistaObjectes(self, request, context):
+        """
+            Retorna la llista d'objectes del camp orderedList
+        """
+
+        llista_ids = []
+        if context.orderedList != [] and context.orderedList != None:
+            for element in context.orderedList:
+                llista_ids.append(element[1])
+
+        return {
+            "Ids" : llista_ids
+        }
 
 
 
 
-class View(grok.View):
+class View(grok.View, funcionsCerca):
     """
         Main View
     """
+
     grok.context(IPlaylist)
     grok.require('zope2.View')
     grok.name('view')
 
+
+
+
+class playlistView(grok.View, resultatsView):
+    """ 
+        Pinta els resultats i les dades referents a la paginacio i les opcions
+        de visualitzacio.
+    """
+
+    grok.context(IPlaylist)
+    grok.require('zope2.View')
+    grok.name('playlistView')
+    grok.template('playlistView')
+
+    def retDadesResultats(self):
+        """ 
+            Crida la vista displayResultatsPaginaView i retorna l'html generat
+        """
+
+        request = self.request
+        request.set('sorting','False')
+
+        portal = getToolByName(self, 'portal_url')
+        portal = portal.getPortalObject()
+        path = '/'.join(self.context.getPhysicalPath())
+        html = portal.restrictedTraverse(path + '/displayResultsPlaylistView')()
+        return html
+
+
+
+
+class displayResultsPlaylistView(grok.View, funcionsCerca):
+    """ pinta l'html corresponent als resultats de la pagina actual (sense
+    controls de visualitzacio)
+    """
+
+    grok.context(IPlaylist)
+    grok.require('zope2.View')
+    grok.name('displayResultsPlaylistView')
+    grok.template('displayResultsPlaylistView')
+
+    def retNumPagina(self):
+        """ retorna el numero de pagina que cal pintar pintar
+        """
+        parametres_visualitzacio = self.retParametresVisualitzacio()
+        return int(parametres_visualitzacio['pagina_actual'])
+
+    def retDades(self):
+        """ retorna l'html dels resultats de la pagina actual
+        (nomes els resultats en si, no els controls de visualitzacio)
+        """
+        #TODO: per testejar descomentar seguent linia i comentar la resta
+        #return self.retNumPagina()
+        portal = getToolByName(self, 'portal_url')
+        portal = portal.getPortalObject()
+        path = '/'.join(self.context.getPhysicalPath())
+        html = portal.restrictedTraverse(path + '/genericPlaylistview')()
+        return html
+
+
+
+
+class genericPlaylistview(grok.View, genericView):
+
+    grok.context(IPlaylist)
+    grok.require('zope2.View')
+    grok.name('genericPlaylistview')
+    grok.template('genericPlaylistview')
+
+    def dades_genericview_header(self):
+        """
+            funcions que retornen les dades necessaries per pintar cada vista
+        """
+        orderedList = self.context.orderedList
+
+        dades_json = self.retSectionHeader()  # retorna diccionari
+        resultat = []
+        if dades_json:
+            i = 0
+            for objecte in dades_json:
+                id_objecte = self.idobjectes[i]
+                i += 1
+                titol_objecte = self.getTitolObjecte(objecte['sections'])
+
+                dades = []
+                for seccio in objecte['sections']:
+                    if seccio['name'] == 'header':
+                        for dada in seccio['data']:
+                            dades.append(self.llegirDada(dada))
+
+                # Treiem l'ordre que te cada element
+                order = 0
+                for obj in orderedList:
+                    if id_objecte == obj[1]:
+                        order = obj[0]
+
+
+                dades_objecte = {'id': id_objecte,
+                                 'titol': titol_objecte,
+                                 'classe': objecte['className'],
+                                 'thumbnail_classe': self.getThumbnailClasse(objecte['className']),
+                                 'thumbnail_objecte': self.getThumbnailObjecte(id_objecte),
+                                 'dades_header': dades,
+                                 'order': order}
+                resultat.append(dades_objecte)
+
+        if resultat != []:
+            # Ordenem els resultats segons l'ordre que s'ha fet des de la pantalla d'ordenacio
+            orderedResultat = sorted(resultat, key=lambda k: int(k['order']))
+            return orderedResultat
+        else:
+            return resultat
+
+
+
+
+class sortingView(grok.View, funcionsCerca):
+    """
+        Sorting View
+    """
+
+    grok.context(IPlaylist)
+    grok.require('zope2.View')
+    grok.name('sortingView')
+    grok.template('sortingView')
+
+
+
+
+class orderPlaylistView(grok.View, resultatsView):
+    """
+        View for sorting Playlist elements
+    """
+
+    grok.context(IPlaylist)
+    grok.require('zope2.View')
+    grok.name('orderPlaylistView')
+    grok.template("orderPlaylistView")
+
+
+    def retDadesResultats(self):
+        """ 
+            Crida la vista displayResultatsPaginaView i retorna l'html generat
+        """
+
+        request = self.request
+        request.set('sorting','True')
+        
+        portal = getToolByName(self, 'portal_url')
+        portal = portal.getPortalObject()
+        path = '/'.join(self.context.getPhysicalPath())
+        html = portal.restrictedTraverse(path + '/displayResultsPlaylistView')()
+        return html
+    
 
 
 
@@ -152,50 +320,44 @@ class updateList(grok.View):
         newOrder = self.request.get('order')
         oldList = context.orderedList
         newList = []
+        index = 0
         for element in newOrder:
             for oldElement in oldList:
                 if str(element) == str(oldElement[0]):
-                    newList.append(oldElement)
+                    newList.append([index, oldElement[1]])
+                    index += 1
 
         if newList != []:
             context.orderedList = newList
-
-            #Check if we have removed elements from the list 
-            #then we update the listToPlay reference field
-            if (len(newList) != len(oldList)):
-                idsList = []
-                for i in newList:
-                    idsList.append(i[1])
-
-                newRelationList = []
-
-                for item in context.listToPlay:
-                    if item.to_object.id in idsList:
-                        newRelationList.append(item)
-
-                context.listToPlay = newRelationList
-
  
     def render(self):
         print 'orderedList field Updated.'
 
 
 
-"""
-class AddForm(dexterity.AddForm):
 
-    grok.name('fatac.playlist')
+class deleteObjectId(grok.View):
+    """
+       View for removing an ID from the orderedList
+    """
 
-    schema = IPlaylist
-    
+    grok.context(IPlaylist)
+    grok.require('zope2.View')
+    grok.name('deleteObjectId')
 
     def update(self):
-        DefaultAddForm.update(self)
+        context = self.context
+        objToDelete = self.request.get('objectId')
+        oldList = context.orderedList
+        newList = []
+        index = 0
+        for oldElement in oldList:
+            if objToDelete != oldElement[1]:
+                newList.append([index, oldElement[1]])
+                index += 1
 
-    def getBlockPlanJSON():
-        return getBlockPlanJSON()
-
-
-class AddView(DefaultAddView):
-    form = AddForm
-"""
+        if newList != []:
+            context.orderedList = newList
+ 
+    def render(self):
+        print 'ID removed successfully'
